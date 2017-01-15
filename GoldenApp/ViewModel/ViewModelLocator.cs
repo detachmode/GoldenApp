c7c85919177673ed6dfaefb954d7e82823ef05fc
Dennis <dennis.briefkasten@gmail.com>
2017-01-15 16:44:29 +0100

    Commit

--- GoldenApp/ViewModel/ViewModelLocator.cs
+++ GoldenApp/ViewModel/ViewModelLocator.cs
@@ -18,9 +18,11 @@ using Microsoft.Practices.ServiceLocation;
 
 namespace GoldenApp.ViewModel
 {
+
     /// <summary>
     /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
+    /// application and 
+    /// provides an entry point for the bindings.
     /// </summary>
     public class ViewModelLocator
     {
